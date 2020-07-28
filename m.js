3378ad701397ae7b750a42cd7f9fb82b1f1e3a61
sinthab23 <68404543+sinthab23@users.noreply.github.com>
2020-07-29 03:28:38 +0700

    Add files via upload

---
+++ m.js
@@ -0,0 +1,22 @@
+&lt;script type=&#39;text/javascript&#39;&gt;
+var uri = window.location.toString();
+if (uri.indexOf(&quot;%3D&quot;,&quot;%3D&quot;) &gt; 0) {
+  var clean_uri = uri.substring(0, uri.indexOf(&quot;%3D&quot;));
+    window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf(&quot;%3D%3D&quot;,&quot;%3D%3D&quot;) &gt; 0) {
+  var clean_uri = uri.substring(0, uri.indexOf(&quot;%3D%3D&quot;));
+    window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf(&quot;&amp;m=1&quot;,&quot;&amp;m=1&quot;) &gt; 0) {
+  var clean_uri = uri.substring(0, uri.indexOf(&quot;&amp;m=1&quot;));
+    window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf(&quot;?m=1&quot;,&quot;?m=1&quot;) &gt; 0) {
+  var clean_uri = uri.substring(0, uri.indexOf(&quot;?m=1&quot;));
+    window.history.replaceState({}, document.title, clean_uri);
+}
+&lt;/script&gt;
